joyunhsiao/joyunhsiao

feat: Complete home page layout, functionality, and RWD
- Finished the design and implementation of the home page, showcasing past works.
- Added navigation menu with smooth transitions.
- Implemented light and dark theme toggle functionality.
- Applied responsive web design (RWD) across the entire website.

src/components/main/main-footer.tsx
@@ -1,0 +1,45 @@
+"use client";
+
+import footer from "@/config/footer";
+import menus from "@/config/menu";
+import { DesktopBottomNav } from "../navigations/desktop";
+import { MobileBottomNav } from "../navigations/mobile";
+
+const MainFooter = () => {
+  return (<>
+    <footer className={`
+      sticky
+      bottom-0
+      z-40
+      border-y-[1.2px]
+      border-stone-300
+      bg-neutral-50
+      shadow-t-sm
+      dark:border-stone-600/50
+      dark:bg-stone-800
+      dark:shadow-stone-900
+    `}>
+      <nav className={`
+        mx-auto
+        hidden
+        max-w-5xl
+        items-center
+        justify-between
+        md:flex
+      `}>
+        <DesktopBottomNav footer={footer} />
+      </nav>
+      <nav className={`
+        mx-auto
+        max-w-5xl
+        items-center
+        justify-between
+        md:hidden
+      `}>
+        <MobileBottomNav menus={menus} />
+      </nav>
+    </footer>
+  </>);
+};
+
+export default MainFooter;

src/components/main/main-header.tsx
@@ -1,0 +1,88 @@
+"use client";
+
+import menus from "@/config/menu";
+import { Disclosure } from "@headlessui/react";
+import { Fragment } from "react";
+import { Logo, Toggle } from "../navigations/core";
+import { DesktopNav } from "../navigations/desktop";
+import { MobileMenuButton, MobileNav } from "../navigations/mobile";
+
+const MainHeader = () => {
+  return (<>
+    <div className={`
+      border-y-1
+      sticky
+      top-0
+      z-50
+      border-black/5
+      bg-neutral-50
+      shadow-sm
+      shadow-neutral-300
+      dark:border-white/10
+      dark:bg-stone-800
+      dark:shadow-stone-900/60
+    `}>
+      <Disclosure as="div">
+        {({ open }) => (
+          <>
+            <nav className={`
+              mx-auto
+              flex
+              max-w-5xl
+              items-center
+              justify-between
+              px-2
+              py-4
+            `}>
+              {/* Logo */}
+              <div className={`
+                flex
+                flex-1
+                justify-start
+                pl-2
+              `}>
+                <Logo />
+              </div>
+
+              {/* Navigation */}
+              <div className={`
+                hidden
+                gap-x-6
+                md:flex
+                md:flex-1
+              `}>
+                <DesktopNav menus={menus} />
+              </div>
+
+              {/* Color Mode */}
+              <div className={`
+                hidden
+                justify-end
+                pr-2
+                md:flex
+                md:flex-1
+              `}>
+                <Toggle />
+              </div>
+              {/* Mobile Menu */}
+              <div className={`
+                flex
+                flex-1
+                justify-end
+                pr-2
+                md:hidden
+              `}>
+                <MobileMenuButton open={open} />
+              </div>
+            </nav>
+
+            {/* <!-- Mobile Navigation --> */}
+            <MobileNav fragment={Fragment} menus={menus} />
+          </>
+        )}
+      </Disclosure>
+    </div>
+  </>);
+};
+
+export default MainHeader;

joyun25/joyun25

src/components/main/main-footer.tsx
@@ -1,0 +1,44 @@
+"use client";
+
+import { footerConfig, menuConfig } from "@/config";
+import { DesktopBottomNav } from "../navigations/desktop";
+import { MobileBottomNav } from "../navigations/mobile";
+
+const MainFooter = () => {
+  return (<>
+    <footer className={`
+      sticky
+      bottom-0
+      z-40
+      border-y-[1.2px]
+      border-stone-300
+      bg-neutral-50
+      shadow-t-sm
+      dark:border-stone-600/50
+      dark:bg-stone-800
+      dark:shadow-stone-900
+    `}>
+      <nav className={`
+        mx-auto
+        hidden
+        max-w-5xl
+        items-center
+        justify-between
+        md:flex
+      `}>
+        <DesktopBottomNav footer={footerConfig} />
+      </nav>
+      <nav className={`
+        mx-auto
+        max-w-5xl
+        items-center
+        justify-between
+        md:hidden
+      `}>
+        <MobileBottomNav menus={menuConfig} />
+      </nav>
+    </footer>
+  </>);
+};
+
+export default MainFooter;

src/components/main/main-header.tsx
@@ -1,0 +1,88 @@
+"use client";
+
+import { Disclosure } from "@headlessui/react";
+import { Fragment } from "react";
+import { Logo, Toggle } from "../navigations/core";
+import { DesktopNav } from "../navigations/desktop";
+import { MobileMenuButton, MobileNav } from "../navigations/mobile";
+import { menuConfig } from "@/config";
+
+const MainHeader = () => {
+  return (<>
+    <div className={`
+      border-y-1
+      sticky
+      top-0
+      z-50
+      border-black/5
+      bg-neutral-50
+      shadow-sm
+      shadow-neutral-300
+      dark:border-white/10
+      dark:bg-stone-800
+      dark:shadow-stone-900/60
+    `}>
+      <Disclosure as="div">
+        {({ open }) => (
+          <>
+            <nav className={`
+              mx-auto
+              flex
+              max-w-5xl
+              items-center
+              justify-between
+              px-2
+              py-4
+            `}>
+              {/* Logo */}
+              <div className={`
+                flex
+                flex-1
+                justify-start
+                pl-2
+              `}>
+                <Logo />
+              </div>
+
+              {/* Navigation */}
+              <div className={`
+                hidden
+                gap-x-6
+                md:flex
+                md:flex-1
+              `}>
+                <DesktopNav menus={menuConfig} />
+              </div>
+
+              {/* Color Mode */}
+              <div className={`
+                hidden
+                justify-end
+                pr-2
+                md:flex
+                md:flex-1
+              `}>
+                <Toggle />
+              </div>
+              {/* Mobile Menu */}
+              <div className={`
+                flex
+                flex-1
+                justify-end
+                pr-2
+                md:hidden
+              `}>
+                <MobileMenuButton open={open} />
+              </div>
+            </nav>
+
+            {/* <!-- Mobile Navigation --> */}
+            <MobileNav fragment={Fragment} menus={menuConfig} />
+          </>
+        )}
+      </Disclosure>
+    </div>
+  </>);
+};
+
+export default MainHeader;